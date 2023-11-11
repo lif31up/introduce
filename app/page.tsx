0b30a4a3ba23b56f76366b196b77d97dbf1f9302
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2023-11-11 19:49:34 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,29 @@ import Quote from "@/app/components/feature/Quote";
 import AnimationContainerTable from "@/app/components/common/AnimationContainerTable";
 import Scene from "@/app/components/feature/Scene";
 import WorkingOnTable from "@/app/components/feature/WorkingOnTable";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBO81lI2Q1Jm0p-mO28UP9rq-c36gY8o6g",
+  authDomain: "webp-1628f.firebaseapp.com",
+  projectId: "webp-1628f",
+  storageBucket: "webp-1628f.appspot.com",
+  messagingSenderId: "238819752790",
+  appId: "1:238819752790:web:198cf02c7431151b72b4f7",
+  measurementId: "G-K73MJ55VCH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 export default function Home() {
   return (
   <main>
