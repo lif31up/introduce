2732161905da7d4ddc650b142a2abb878d28da5f
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2023-11-11 20:02:14 +0900

    Update Firebase.tsx

--- app/components/common/Firebase.tsx
+++ app/components/common/Firebase.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import {useEffect} from "react";
 const firebaseConfig = {
 	apiKey: "AIzaSyBO81lI2Q1Jm0p-mO28UP9rq-c36gY8o6g",
 	authDomain: "webp-1628f.firebaseapp.com",
@@ -10,8 +11,10 @@ const firebaseConfig = {
 	appId: "1:238819752790:web:198cf02c7431151b72b4f7",
 	measurementId: "G-K73MJ55VCH"
 };
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export default function Firebase({}){
+	useEffect(()=>{
+		const app = initializeApp(firebaseConfig);
+		const analytics = getAnalytics(app);
+	},[false]);
 	return(<></>);
 }
