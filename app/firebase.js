2137a79503cb59a60308a8d6ec2abef67fc8de28
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2023-11-12 15:07:34 +0900

    updated whole

---
+++ app/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBO81lI2Q1Jm0p-mO28UP9rq-c36gY8o6g",
+	authDomain: "webp-1628f.firebaseapp.com",
+	projectId: "webp-1628f",
+	storageBucket: "webp-1628f.appspot.com",
+	messagingSenderId: "238819752790",
+	appId: "1:238819752790:web:198cf02c7431151b72b4f7",
+	measurementId: "G-K73MJ55VCH"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export default analytics;
