d04a81b6ae8379456b809d4a649ab5d6e69de2bf
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2023-11-14 01:03:26 +0900

    Update Caption.tsx

--- app/components/feature/Caption.tsx
+++ app/components/feature/Caption.tsx
@@ -1,6 +1,6 @@
 export default function Caption({}){
 	return(
-	<div className={"_content p-4 custom-font-gothic-a1-thin"}>
+	<div className={"_content p-4 custom-font-gothic-a1-thin custom-mobile-disable"}>
 		<p><div className={"inline-block bg-white custom-sphere h-2 w-2 mr-2"}/>{"현재 팀 프로젝트 과정에 있습니다."}</p>
 	</div>
 	);
