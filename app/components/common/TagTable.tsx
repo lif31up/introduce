cf296ed0cd3dcec591217a80d8d0315d96ede2c2
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2023-11-13 18:39:07 +0900

    Update TagTable.tsx

--- app/components/common/TagTable.tsx
+++ app/components/common/TagTable.tsx
@@ -3,7 +3,7 @@ import "@/styles/Common.css";
 export default function TagTable({children,color}:{children:Array<string>,color:string}){
 	const tags:Array<React.ReactNode> = [];
 	children.forEach((child,index)=>{
-		tags.push(<h1 key={index} className={["custom-rounded inline-block mr-4 my-2 w-fit",color].join(" ").trim()}>{child}</h1>)
+		tags.push(<h1 key={index} className={["custom-rounded inline-block mr-4 my-2 w-fit custom-font-gothic-a1",color].join(" ").trim()}>{child}</h1>)
 	});
 	return(
 	<div className={"_trans_content block"}>{tags}</div>
