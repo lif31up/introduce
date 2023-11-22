28e54f72611e22ae76dd50ccbed3fa67dc8f4dd4
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2023-11-22 22:11:19 +0900

    Update WorkingOn.tsx

--- app/components/feature/WorkingOn.tsx
+++ app/components/feature/WorkingOn.tsx
@@ -8,8 +8,8 @@ export default function WorkingOn({ subject, desc }: WorkingOnProps) {
   return (
     <div className={'_grid-content _bg-black-045 text-white pt-4 _rounded-1xl'}>
       <div className={'block pb-4'}>
-        <h1 className={'_heading_regular'}>{subject}</h1>
-        <p className={"_text_small text-neutral-400"}>{desc}</p>
+        <h1 className={'_heading_large'}>{subject}</h1>
+        <p className={"_text_regular text-neutral-400"}>{desc}</p>
       </div>
       <div className={'loading-bar'} />
     </div>
