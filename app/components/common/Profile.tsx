8df3712ade6d20d6568c9758fa85fb73920e4397
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2023-11-23 23:41:43 +0900

    Update Profile.tsx

--- app/components/common/Profile.tsx
+++ app/components/common/Profile.tsx
@@ -12,7 +12,7 @@ export default function Profile({isMobile}:{isMobile:boolean}){
         <Image src={"/assets/images/profile.webp"} alt={"profile"} width={0} height={0} sizes={"60px"} className={"w-24 h-24 _sphere"}/>
         <div className={"pl-8"}>
           <h1 className={"_heading_large"}>한명환</h1>
-          <p className={"_text_regular text-neutral-400"}>연세대학교 미래캠퍼스 컴퓨터공학과 (중퇴)</p>
+          <p className={"_text_regular text-neutral-400"}>연세대학교 미래캠퍼스 컴퓨터공학과</p>
           <p className={"_text_regular text-neutral-400"}>NULRANLCNS9991@GMAIL.COM</p>
           <p className={"_text_regular text-neutral-400"}>현재 성남에서 거주 중입니다</p>
           <ul className={"pt-4 _text_regular text-emerald-300 flex gap-4"}>
