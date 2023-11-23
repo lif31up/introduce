b2c18d580e276add3347a3792b42485fa2c1024a
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2023-11-23 23:44:17 +0900

    Update TopBar.tsx

--- app/components/common/TopBar.tsx
+++ app/components/common/TopBar.tsx
@@ -28,7 +28,7 @@ export default function TopBar({height}:TopBarProps){
     <>
       <div className={"_bg-black-045 flex items-center pl-4 pr-8 justify-between select-none"} style={TopBarStyle} id={"top-bar--0"}>
         <div className={"flex gap-4 items-center"}>
-          <div className={"w-8 h-8 _sphere bg-neutral-500 _mobile_disable"}/>
+          <div className={"w-11 h-11 _sphere bg-neutral-500"}/>
           <Link onClick={():void=>{logHandler("github_account")}} href={"https://github.com/lif31up"} className={"rounded-3xl px-6 _py-05 bg-purple-600 _heading_regular shadow-black _desktop_disable text-neutral-200"}>GITHUB ACCOUNT</Link>
           <h1 className={"text-neutral-400 -mb-1 _heading_regular _mobile_disable"}>명환의 포트폴리오에 오신 걸 환영합니다.</h1>
         </div>
