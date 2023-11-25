8d46f9472681b82c351123af62ac2a35793263ed
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2023-11-26 04:27:50 +0900

    만들기

--- app/components/common/Heading.tsx
+++ app/components/common/Heading.tsx
@@ -10,7 +10,7 @@ export default function Heading({children,desc,noPadding}:{children:string,desc:
     descNode = <p className={"_heading_small text-neutral-400"} style={{fontWeight: 400}}>{desc}</p>
   }
   return(
-    <div className={["mx-8 mt-8",noPadding ? "":"mb-16"].join(" ").trim()}>
+    <div className={["mx-8 mt-16",noPadding ? "":"mb-8"].join(" ").trim()}>
       <h1 className={"-mb-8 _heading_huge text-white"} style={headingStyle}>
         {children}
       </h1>
