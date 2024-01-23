18fa102b55cfc1ffdd170d4680f365af190f6280
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2024-01-23 19:08:34 +0900

    ProjectBoard의 ProjectCard의 내용과 순서를 변경했습니다.

--- component/section/ProjectBoard.tsx
+++ component/section/ProjectBoard.tsx
@@ -21,16 +21,6 @@ export default function ProjectBoard({ className, id, title }: Default): React.J
         <ObservedDistributor prefix='left--' animation={'_anime-slp-ltr-slide'} threshold={0.1}>
           <ProjectCard
             data={{
-              title: '포트폴리오',
-              heading: '구인구직을 위해 만든 개인 페이지',
-              desc: '현재 보고계시는 페이지입니다. 제가 지금 것 해왔던 프로젝트를 종합하기 위해 만들었습니다. 화려한 에니메이션을 통해 더 인상깊은 사용자 경험을 만들 수 있었습니다.',
-              tags: ['리액트', '넥스트', '베르셀', '파이어베이스', 'SCSS'],
-              href: 'https://introduce-murex.vercel.app/',
-              repo: 'https://github.com/lif31up/introduce',
-            }}
-          />
-          <ProjectCard
-            data={{
               title: '자료구조',
               heading: '프로그래밍의 기초 중에 기초',
               desc: 'C언어를 이용해 연결 리스트, 이중 탐색 트리, 그래프, 세트, AVL트리 등을 구현해보았습니다. 프로그래밍 언어에 대한 전반적인 지식과 감각을 잡는데 큰 도움을 준 프로젝트입니다.',
@@ -41,12 +31,22 @@ export default function ProjectBoard({ className, id, title }: Default): React.J
           />
           <ProjectCard
             data={{
-              title: '멀티 블로그',
-              heading: '다사다난 했던 협업 경험...',
-              desc: '현재 제가 수행하고 있는 팀 프로젝트입니다. 캠퍼스픽이라는 어플을 통해서 각 분야의 학생이 모여서 블로그 서비스를 제작 중입니다. 현재까지 이는 아직 진행 중입니다. 클릭해서 UI/UX와 디자인 체계 설계 과정을 볼 수 있습니다.',
-              tags: ['피그마', '슬랙', '리액트', '테일윈드 CSS'],
-              href: 'https://github.com/devBlogProject/front-end',
-              repo: 'https://github.com/devBlogProject/front-end',
+              title: '무한 스크롤',
+              heading: '쇼핑몰에 사용될 수 있는 무한 스크롤링',
+              desc: '리액트 쿼리를 배우는 과정에서 만들게 된 기능입니다. Observer API와 리액트 후크를 통해 에러가 나지 않고 재사용 가능한 무한 스크롤러를 만들었습니다.',
+              tags: ['리액트 쿼리', '리액트', '후크', '패칭'],
+              href: 'https://shopping-mall-rust.vercel.app/',
+              repo: 'https://github.com/lif31up/infinite-scroll',
+            }}
+          />
+          <ProjectCard
+            data={{
+              title: '포트폴리오',
+              heading: '구인구직을 위해 만든 개인 페이지',
+              desc: '현재 보고계시는 페이지입니다. 제가 지금 것 해왔던 프로젝트를 종합하기 위해 만들었습니다. 화려한 에니메이션을 통해 더 인상깊은 사용자 경험을 만들 수 있었습니다.',
+              tags: ['리액트', '넥스트', '베르셀', '파이어베이스', 'SCSS'],
+              href: 'https://introduce-murex.vercel.app/',
+              repo: 'https://github.com/lif31up/introduce',
             }}
           />
         </ObservedDistributor>
@@ -55,19 +55,19 @@ export default function ProjectBoard({ className, id, title }: Default): React.J
         <ObservedDistributor prefix='right--' animation={'_anime-slp-rtl-slide'} threshold={0.1}>
           <ProjectCard
             data={{
-              title: '파이어베이스',
-              heading: '외부 API를 공부하고 사용해본 경험',
-              desc: '현재 제가 수행하고 있는 팀 프로젝트입니다. 캠퍼스픽이라는 어플을 통해서 각 분야의 학생이 모여서 블로그 서비스를 제작 중입니다. 현재까지 이는 아직 진행 중입니다. 클릭해서 UI/UX와 디자인 체계 설계 과정을 볼 수 있습니다.',
-              tags: ['파이어베이스', '리액트', '넥스트'],
-              href: 'https://firebase-gamma-three.vercel.app/',
-              repo: 'https://github.com/lif31up/firebase',
+              title: '멀티 블로그',
+              heading: '다사다난 했던 협업 경험...',
+              desc: '현재 제가 수행하고 있는 팀 프로젝트입니다. 캠퍼스픽이라는 어플을 통해서 각 분야의 학생이 모여서 블로그 서비스를 제작 중입니다. 저는 UI/UX 기획 및 디자인과 프론트엔드 개발을 수행했습니다.',
+              tags: ['피그마', '슬랙', '리액트', '테일윈드 CSS'],
+              href: 'https://github.com/devBlogProject/front-end',
+              repo: 'https://github.com/devBlogProject/front-end',
             }}
           />
           <ProjectCard
             data={{
               title: '에니메이션 실험소',
               heading: '웹 에니메이션을 능숙하게 다룹니다',
-              desc: '현재 제가 수행하고 있는 팀 프로젝트입니다. 캠퍼스픽이라는 어플을 통해서 각 분야의 학생이 모여서 블로그 서비스를 제작 중입니다. 현재까지 이는 아직 진행 중입니다. 클릭해서 UI/UX와 디자인 체계 설계 과정을 볼 수 있습니다.',
+              desc: '더 나은 사용자 경험을 위해서 에니메이션이 사용될 수도 있습니다. 그래서 에니메이션 실험소를 만들어 CSS 에니메이션 또는 DOM API를 공부하고 기록하고 있습니다.',
               tags: ['Observer API', '넥스트', '리액트', 'CSS 에니메이션'],
               href: 'https://animation-nine-weld.vercel.app/',
               repo: 'https://github.com/lif31up/animation',
@@ -75,12 +75,12 @@ export default function ProjectBoard({ className, id, title }: Default): React.J
           />
           <ProjectCard
             data={{
-              title: '무한 스크롤',
-              heading: '쇼핑몰에 사용될 수 있는 무한 스크롤링',
-              desc: '현재 제가 수행하고 있는 팀 프로젝트입니다. 캠퍼스픽이라는 어플을 통해서 각 분야의 학생이 모여서 블로그 서비스를 제작 중입니다. 현재까지 이는 아직 진행 중입니다. 클릭해서 UI/UX와 디자인 체계 설계 과정을 볼 수 있습니다.',
-              tags: ['리액트 쿼리', '리액트', '후크', '패칭'],
-              href: 'https://shopping-mall-rust.vercel.app/',
-              repo: 'https://github.com/lif31up/infinite-scroll',
+              title: '파이어베이스',
+              heading: '외부 API를 공부하고 사용해본 경험',
+              desc: '구글 인증 로그인을 만들고 싶어서 파이어베이스를 배우던 중에 다른 것들도 공부해야겠다 싶어서 만들게 된 사이트입니다. 파이어베이스 대부분의 기능이 한 사이트에 정리되어 있습니다.',
+              tags: ['파이어베이스', '리액트', '넥스트'],
+              href: 'https://firebase-gamma-three.vercel.app/',
+              repo: 'https://github.com/lif31up/firebase',
             }}
           />
         </ObservedDistributor>
