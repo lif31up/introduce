4b8ff1156f4c7e32256fd0a6ecbfcab8653b03a5
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2024-05-03 21:26:03 +0900

    feature: add ObserverContainer
    they observe viewer's sight, then trigger the animation.

---
+++ components/common/ObserverContainer.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import DefaultProps from '@/utils/interface'
+import React, { useEffect, useRef } from 'react'
+
+interface ObserverContainerProps extends DefaultProps<never> {
+  children: React.ReactNode
+  animation: string
+  threshold?: number
+  id: string
+}
+function ObserverContainer({ children, id, animation, threshold }: ObserverContainerProps) {
+  const observerRef = useRef<IntersectionObserver | null>(null)
+  const option = { root: null, rootMargin: '0px', threshold: threshold ? threshold : 0 }
+
+  useEffect(() => {
+    let element: HTMLElement | null = document.getElementById(id)
+    if (!element) return
+    const intersectionHandler = (entries: Array<IntersectionObserverEntry>) => {
+      entries.forEach((entry: IntersectionObserverEntry) => {
+        if (entry.isIntersecting) {
+          if (!element) return
+          element.style.animationPlayState = 'running'
+          return () => {
+            if (!observerRef.current) return
+            observerRef.current.disconnect()
+          }
+        }
+      })
+    }
+    observerRef.current = new IntersectionObserver(intersectionHandler, option)
+    observerRef.current?.observe(element)
+  }, [])
+  return (
+    <div id={id} className={`${animation}`}>
+      {children}
+    </div>
+  )
+}
+export default ObserverContainer
